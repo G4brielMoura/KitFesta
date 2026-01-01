e03d0efd646a6ae731990fe81f0142ed44f30bcd
G4brielMoura <pereirbiel@gmail.com>
2026-01-01 18:33:00 -0300

    delite

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,12 +2,12 @@
 import { Header } from "../_components/header"
 import Footer from "../_components/footer"
 import PageWrapper from "../_components/pagewrapper"
-import Aboutme from "../_components/aboutme"
+
 export default function About() {
   return (
     <PageWrapper>
       <Header />
-      <Aboutme />
+      
       
     </PageWrapper>
   )
